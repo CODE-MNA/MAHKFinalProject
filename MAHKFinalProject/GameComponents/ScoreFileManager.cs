61943c636dad5735a2f3a3a6609fa19e6cb99e2c
rio0118 <rio0118@gmail.com>
2022-12-10 13:46:27 -0500

    modified scoreFileManager (error handling)

--- MAHKFinalProject/GameComponents/ScoreFileManager.cs
+++ MAHKFinalProject/GameComponents/ScoreFileManager.cs
@@ -9,34 +9,65 @@ namespace MAHKFinalProject.GameComponents
 {
     public class ScoreFileManager
     {
-        private const string filepath = "Scores/score.dat";
+        private const string filepath = @"Scores/score.dat";
 
         public void recordScore(int score)
         {
-            using (StreamWriter sw = new StreamWriter(filepath))
+            try
             {
-                sw.WriteLine(score);
+                // if there is no directory, create directory
+                if (!Directory.Exists(filepath))
+                {
+                    Directory.CreateDirectory(Path.GetDirectoryName(filepath));
+                }
+
+                // write file with append mode
+                using (StreamWriter sw = new StreamWriter(filepath, true))
+                {
+                    sw.WriteLine(score);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
             }
         }
 
         public List<int> getHighScores(int numberOfRanks)
         {
-            using (StreamReader sr = new StreamReader(filepath))
+            try
             {
-                string content = sr.ReadToEnd();
+                // if there is no score file, it will return empty list
+                if (File.Exists(filepath))
+                {
+                    using (StreamReader sr = new StreamReader(filepath))
+                    {
+                        string content = sr.ReadToEnd();
 
-                List<string> scoreTexts = new List<string>();
-                List<int> scores = new List<int>();
-                List<int> ranks = new List<int>();
+                        List<string> scoreTexts = new List<string>();
+                        List<int> scores = new List<int>();
+                        List<int> ranks = new List<int>();
 
-                if (!string.IsNullOrEmpty(content)) {
-                    scoreTexts = content.Split("\n").ToList();
-                    scores = scoreTexts.Select(int.Parse).ToList();
-                    ranks = scores.OrderByDescending(s => s).Take(numberOfRanks).ToList();
+                        if (!string.IsNullOrEmpty(content))
+                        {
+                            scoreTexts = content.Split("\n").ToList();
+                            scores = scoreTexts.Select(int.Parse).ToList();
+                            ranks = scores.OrderByDescending(s => s).Take(numberOfRanks).ToList();
+                        }
+
+                        return ranks;
+                    }
                 }
-
-                return ranks;
+                else
+                {
+                    return new List<int>();
+                }
             }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
         }
     }
 }
